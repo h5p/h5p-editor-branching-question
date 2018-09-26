64d7cbaf835f56fb6ecfe5541b2e40ba74afbb9d
Oliver Tacke <o.tacke@posteo.de>
2018-09-26 19:05:23 +0200

    HFP-2246 Change feedback field description

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ H5PEditor.widgets.branchingQuestion = H5PEditor.BranchingQuestion = (function ($
         const description = document.createElement('div');
         description.classList.add('h5p-feedback-description');
         description.classList.add('h5peditor-field-description');
-        description.textContent = 'It is recommended to provide a feedback after each alternative. This will ensure a better learning experience for the viewer.';
+        description.textContent = 'It is recommended to provide feedback that motivates and also provides guidance. Leave all fields empty if you don\'t want the user to get feedback after choosing this alternative/viewing this content.';
 
         const groupWrapper = feedbackGroup.querySelector('.content');
         groupWrapper.prepend(description);
